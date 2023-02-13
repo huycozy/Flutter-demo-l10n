be1695845dee18aa1ae418e6491ec84a81732d1b
huycozy <huy@nevercode.io>
2023-02-13 18:44:11 +0700

    init
    Signed-off-by: huycozy <huy@nevercode.io>

---
+++ android/app/src/main/kotlin/com/example/reproduce_issue_gen_l10n/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reproduce_issue_gen_l10n
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:test_gen_l10n/l10n/app_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
+      home: const DemoHome(),
+    );
+  }
+}
+
+class DemoHome extends StatelessWidget {
+  const DemoHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            // Returns 'Hello John'
+            Text(AppLocalizations.of(context).helloWorld),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
